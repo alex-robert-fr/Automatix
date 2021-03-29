823c2bd856c24c0ac1147c897a9750894e7aac28
AxDesign <contact.alexrobert04@gmail.com>
2021-03-29 09:07:07 +0200

    FEAT - Le fichier .txt est maintenant convertie en objet JSON

--- jsx/extendScript.jsx
+++ jsx/extendScript.jsx
@@ -3,15 +3,15 @@ $.runScript = {
 
 
 	CreateMarker: function() {
-		for(i = 0; i < jsonJSX.chapitre.length; i++){
+		for(i = 0; i < jsonJSX.chapitres.length; i++){
 
-			var time = jsonJSX.chapitre[i].time;
+			var time = jsonJSX.chapitres[i].time;
 			var secondes = time.charAt(time.length-2) + time.charAt(time.length-1);
 			var minutes = time.charAt(time.length-5) + time.charAt(time.length-4);
 
 			var markers = app.project.activeSequence.markers;
 			var newMarkers = markers.createMarker(parseInt(secondes) + parseInt(minutes)*60);
-			newMarkers.name = jsonJSX.chapitre[i].description;
+			newMarkers.name = jsonJSX.chapitres[i].description;
 		}
 
 	}
